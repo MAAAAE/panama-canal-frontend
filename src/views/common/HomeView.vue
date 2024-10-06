4d0ac464784a20b507f89a953b221cc2d01cbc2c
d36choi <yuio1126@gmail.com>
2024-10-06 17:12:43 +0900

    refactor: demo용 대시보드 ui 영역 제거
    - 거래랑 클라이언트 정보였음

--- src/views/common/HomeView.vue
+++ src/views/common/HomeView.vue
@@ -5,15 +5,11 @@ import {
   mdiApplicationOutline,
   mdiChartTimelineVariant,
   mdiCounter,
-  mdiGithub,
   mdiHeartBox,
 } from '@mdi/js';
 import * as chartConfig from '@/components/Charts/chart.config.js';
 import SectionMain from '@/components/SectionMain.vue';
 import CardBoxWidget from '@/components/CardBoxWidget.vue';
-import BaseButton from '@/components/BaseButton.vue';
-import CardBoxTransaction from '@/components/CardBoxTransaction.vue';
-import CardBoxClient from '@/components/CardBoxClient.vue';
 import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue';
 import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue';
 import {
@@ -33,10 +29,8 @@ onMounted(() => {
 });
 
 const mainStore = useMainStore();
-
-const clientBarItems = computed(() => mainStore.clients.slice(0, 4));
-
-const transactionBarItems = computed(() => mainStore.history);
+computed(() => mainStore.clients.slice(0, 4));
+computed(() => mainStore.history);
 </script>
 
 <template>
