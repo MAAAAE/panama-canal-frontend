19f343e396f76749cf7318f7646462ca19641632
Marc Lee <chlee1252@gmail.com>
2024-10-16 23:50:21 +0900

    feat: remove token for gateway

--- src/gatewayClient.js
+++ src/gatewayClient.js
@@ -11,12 +11,6 @@ const gatewayClient = axios.create({
 // 요청 인터셉터를 추가하여 Authorization 헤더를 설정합니다.
 gatewayClient.interceptors.request.use(
   (config) => {
-    // 로컬 스토리지 또는 다른 저장소에서 토큰을 가져옵니다.
-    const token = keycloak.token;
-    if (token) {
-      // Authorization 헤더에 토큰을 추가합니다.
-      config.headers.Authorization = `Bearer ${token}`;
-    }
     return config;
   },
   (error) => {
